dbe2d2b1a09f0071faa3e542c081fe10e5144348
othman amir <othmanskipper17@gmail.com>
2023-11-02 05:39:40 +0100

    projet java script

---
+++ index.js
@@ -0,0 +1,21 @@
+//Exercise 1:
+function somme(a,b){
+    return a+b;
+}
+console.log(somme(3,6));
+console.log(somme(-2,-4));
+
+//Exercise 2:
+function mintosec(a){
+    let sec = a*60;
+    return sec;
+}
+console.log(mintosec(4));
+console.log(mintosec(3));
+
+//Exercise 3:
+function increment(a){
+ return a+1;
+}
+console.log(increment(1));
+console.log(increment(4));
